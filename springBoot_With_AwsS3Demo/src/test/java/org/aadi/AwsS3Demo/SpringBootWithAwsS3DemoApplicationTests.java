984e906e41641d6b503b9c261f86fa6e36b1de20
aadi <aadiraj48@gmail.com>
2021-01-15 10:30:38 +0530

    Boot with AWS S3 Demo - To upload/download/delete files from s3 bucket

---
+++ springBoot_With_AwsS3Demo/src/test/java/org/aadi/AwsS3Demo/SpringBootWithAwsS3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aadi.AwsS3Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithAwsS3DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
